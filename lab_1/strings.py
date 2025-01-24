3f66345754815c40491969013e8676a89d61646c
@malika-bimuratova-brq6837 <malikabimurat@gmail.com>
2025-01-24 19:28:08 +0300

    lab 1 solved

---
+++ lab_1/strings.py
@@ -0,0 +1,35 @@
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+b = "Hello, World!"
+print(b[2:5])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
---
+++ lab_1/variables.py
@@ -0,0 +1,26 @@
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+myvar = "Salavat"
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
